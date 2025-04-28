15ee3a23cd3ad67f6f63020322807e9f91c8c99a
Amaan Sheikh <94191143+amaan-123@users.noreply.github.com>
2025-04-28 15:07:29 +0530

    add files of Fetch API Asg 4 Content Check using Azure Content Safety API

---
+++ FetchAPI/Project/Assignments/asg4/script.js
@@ -0,0 +1,59 @@
+const submitBtn = document.getElementById("submitBtn");
+submitBtn.addEventListener("click", checkContent);
+
+
+async function checkContent() {
+    const text = document.getElementById('userInput').value.trim();
+    console.log("User Input: ", text);
+    console.log("JSON-encoded: ",JSON.stringify({text}));
+
+    const endpoint = 'https://safetydemo.cognitiveservices.azure.com';
+    const apiKey = 'EPaXpby9VcEuhcFJqktPtJGCMCekRzFwmHLvy29YsosUBcyo3mRFJQQJ99BDACYeBjFXJ3w3AAAHACOG3Ay8';
+    const apiVersion = '2024-09-01';
+
+    const url = `${endpoint}/contentsafety/text:analyze?api-version=${apiVersion}`;
+    console.log("URL is: ", url);
+
+    if (!text) {
+      console.error("Comment cannot be empty.");
+      return;
+    }
+    try {
+      const response = await fetch(url, {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json',
+              'Ocp-Apim-Subscription-Key': apiKey
+          },
+          body: JSON.stringify({text})
+      });
+
+      const data = await response.json();
+      console.log("API Response:", data); // Log the response to inspect its structure
+
+      const categoriesAnalysis = data.categoriesAnalysis;
+
+      if (Array.isArray(categoriesAnalysis)) {
+        const table = document.getElementById('result');
+        table.innerHTML = "";
+        categoriesAnalysis.forEach(item => {
+          const row = document.createElement('tr');
+          const categoryData = document.createElement('td');
+          const severityData = document.createElement('td');
+
+          categoryData.innerHTML = item.category;
+          severityData.innerHTML = item.severity;
+
+          row.appendChild(categoryData);
+          row.appendChild(severityData);
+          table.appendChild(row);
+        });
+      } else {
+        console.error("Unexpected data structure for categoriesAnalysis:", categoriesAnalysis);
+      }
+      
+    }
+    catch (error) {
+      console.error("Error during API call:", error);
+    }
+}
