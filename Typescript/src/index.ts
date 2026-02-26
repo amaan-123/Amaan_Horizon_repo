0428951761b27153f4a86529b4d4ff5ad2d6cd7f
Amaan Sheikh <94191143+amaan-123@users.noreply.github.com>
2026-02-26 12:11:44 +0530

    Start: Learning Typescript

---
+++ Typescript/src/index.ts
@@ -0,0 +1,4 @@
+function greet(person: string): string {
+  return `hello ${person}`;
+}
+console.log(greet("Zarar"));
---
+++ Typescript/src/typesinTs.ts
@@ -0,0 +1,4 @@
+let drink = "chai";
+let cups = Math.random() > 0.5 ? 10 : "5";
+console.log(drink);
+console.log(cups);
